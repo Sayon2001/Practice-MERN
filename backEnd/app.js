0f00147ec68c19cf50c630c9bcf2074f7e082cab
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-09-12 15:16:13 +0545

    multerConfig

--- backEnd/app.js
+++ backEnd/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./database')
 const app = express()
+const {multer,storage} = require('./middleware/multerConfig')
+const upload  = multer({storage: storage})
 const Blog = require('./model/blogModel')
 
 connectToDatabase()
@@ -14,19 +16,23 @@ app.get('/',(request,response)=>{
     })
 })
 
-app.post("/blog",async(request,response)=>{
-    const {title,subtitle,description,image} = request.body
-    if(!title || !subtitle || !description || !image){
-        return response.status(400).json({
-            message: 'All fields are required'
-        })  // Returning 400 Bad Request if required fields are missing.
-    }
-    await Blog.create({
-        title : title,
-        subtitle : subtitle,
-        description : description,
-        image : image
-    })
+app.post("/blog", upload.single('image'), async(req,response)=>{
+    // const {title,subtitle,description,image} = request.body
+    // if(!title || !subtitle || !description || !image){
+    //     return response.status(400).json({
+    //         message: 'All fields are required'
+    //     })  // Returning 400 Bad Request if required fields are missing.
+    // }
+    // await Blog.create({
+    //     title : title,
+    //     subtitle : subtitle,
+    //     description : description,
+    //     image : image
+    // })
+    console.log(req.body);
+    console.log(req.file);
+    
+    
     response.status(200).json({
         message: 'Blog api hit successfully'
     })   
