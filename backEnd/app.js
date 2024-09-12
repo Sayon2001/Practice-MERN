f93dc16a4decd0280b457b8cb561fdd1ed3f376d
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-09-12 16:15:34 +0545

    handling file using multer

--- backEnd/app.js
+++ backEnd/app.js
@@ -16,27 +16,35 @@ app.get('/',(request,response)=>{
     })
 })
 
-app.post("/blog", upload.single('image'), async(req,response)=>{
-    // const {title,subtitle,description,image} = request.body
-    // if(!title || !subtitle || !description || !image){
-    //     return response.status(400).json({
-    //         message: 'All fields are required'
-    //     })  // Returning 400 Bad Request if required fields are missing.
-    // }
-    // await Blog.create({
-    //     title : title,
-    //     subtitle : subtitle,
-    //     description : description,
-    //     image : image
-    // })
-    console.log(req.body);
-    console.log(req.file);
-    
-    
+app.post("/blog", upload.single('image'), async(request,response)=>{
+    const {title,subtitle,description} = request.body
+    const filename = request.file.filename
+    if(!title || !subtitle || !description){
+        return response.status(400).json({
+            message: 'All fields are required'
+        })  // Returning 400 Bad Request if required fields are missing.
+
+    }
+    await Blog.create({
+        title : title,
+        subtitle : subtitle,
+        description : description,
+        image : filename
+    })
     response.status(200).json({
         message: 'Blog api hit successfully'
     })   
 })
+
+app.get('/blog',async(request, response) => {
+    const blogs = await Blog.find()
+    response.status(200).json({
+        message: 'Blogs fetched successfully',
+        data:blogs
+    })
+})
+
+app.use(express.static('./storage'))
 
 app.listen(process.env.PORT,()=>{
     console.log('NodeJs project has started')
