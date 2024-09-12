b245e7428762540a47660d5d7cfaf5d222e824ae
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-09-12 16:57:44 +0545

    get single, and delete

--- backEnd/app.js
+++ backEnd/app.js
@@ -37,10 +37,32 @@ app.post("/blog", upload.single('image'), async(request,response)=>{
 })
 
 app.get('/blog',async(request, response) => {
-    const blogs = await Blog.find()
+    const blogs = await Blog.find() //array ma return garxa
     response.status(200).json({
         message: 'Blogs fetched successfully',
         data:blogs
+    })
+})
+
+app.get('/blog/:id',async(request, response) => {
+    const id = request.params.id
+    const blog = await Blog.findById(id) //object ma return garxa
+    if(!blog){
+        return response.status(404).json({
+            message: 'Blog not found'
+        })
+    }
+    response.status(200).json({
+        message: 'Blog fetched successfully',
+        data: blog
+    })
+})
+
+app.delete('/blog/:id',async (request, response) => {
+    const id = request.params.id
+    await Blog.findByIdAndDelete(id)
+    response.status(200).json({
+        message: 'Blog deleted successfully'
     })
 })
 
