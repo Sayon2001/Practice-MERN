482f4c05f0f3539650e833e7afc9bf449d084c1b
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-12-09 17:53:51 +0545

    created a project

---
+++ mernP1/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-blue-500 m-20 text-2xl font-bold'>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
