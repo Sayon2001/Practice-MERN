74608a334a100b7ddedb0f50bf04e65d721124fe
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-12-12 15:38:10 +0545

    get singleblog, delete

--- mernP1/src/components/SingleBLog.jsx
+++ mernP1/src/components/SingleBLog.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
+import { useParams, useNavigate } from 'react-router-dom'
 
 const SingleBLog = () => {
+
+    const { id } = useParams();
+
+    const navigate = useNavigate();
+
+    const [blog, setBlog] = useState({});
+
+    const fetchSingleBlog = async () => {
+        const response = await axios.get("http://localhost:3000/blog/" + id)
+        setBlog(response.data.data)
+    }
+
+    const deleteOnclick = async () => {
+        const response = await axios.delete("http://localhost:3000/blog/" + id);
+        (response.status === 200) ? navigate("/") : alert("Something went wrong");
+
+    }
+
+    useEffect(() => {
+        fetchSingleBlog()
+    }, [])
+
     return (
         <>
             <Navbar />
@@ -10,32 +34,27 @@ const SingleBLog = () => {
                     <div className="flex flex-col md:flex-row -mx-4">
                         <div className="md:flex-1 px-4">
                             <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                                <img className="w-full h-full object-cover" src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg" alt="Product Image" />
+                                <img className="w-full h-full object-cover" src={"http://localhost:3000/" + blog.image} alt="Blog Image" />
                             </div>
                             <div className="flex -mx-2 mb-4">
                                 <div className="w-1/2 px-2">
                                     <button className="w-full bg-blue-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Blog</button>
                                 </div>
                                 <div className="w-1/2 px-2">
-                                    <button className="w-full bg-red-600 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Delete Blog</button>
+                                    <button className="w-full bg-red-600 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteOnclick}>Delete Blog</button>
                                 </div>
                             </div>
                         </div>
                         <div className="md:flex-1 px-4">
-                            <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">Product Name</h2>
-                            <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                                ante justo. Integer euismod libero id mauris malesuada tincidunt.
+                            <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog.title}</h2>
+                            <p className="text-gray-600 dark:text-gray-300 text-lg mb-4">
+                                {blog.subtitle}
                             </p>
 
                             <div>
-                                <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
+                                <span className="font-bold text-gray-700 dark:text-gray-300">Blog Description:</span>
                                 <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                                    sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
-                                    lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit. Quisque
-                                    ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia, non
-                                    sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi consectetur.
+                                    {blog.description}
                                 </p>
                             </div>
                         </div>
