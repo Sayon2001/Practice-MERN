53a1c4455d5085bf89d63d8fa588a4e137d73411
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-12-14 22:04:04 +0545

    Edit Blog Api Integration

--- mernP1/src/App.jsx
+++ mernP1/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SingleBLog from './pages/SingleBLog'
 import CreateBlog from './pages/CreateBlog'
+import EditBlog from './pages/EditBlog'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/create' element={<CreateBlog />} />
         <Route path='/blog/:id' element={<SingleBLog />} />
+        <Route path='/edit/:id' element={<EditBlog />} />
       </Routes>
     </BrowserRouter>
   )
--- mernP1/src/pages/SingleBLog.jsx
+++ mernP1/src/pages/SingleBLog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import axios from 'axios'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate, Link } from 'react-router-dom'
 
 const SingleBLog = () => {
 
@@ -38,7 +38,9 @@ const SingleBLog = () => {
                             </div>
                             <div className="flex -mx-2 mb-4">
                                 <div className="w-1/2 px-2">
-                                    <button className="w-full bg-blue-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Blog</button>
+                                    <Link to={`/edit/${id}`}>
+                                        <button className="w-full bg-blue-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Blog</button>
+                                    </Link>
                                 </div>
                                 <div className="w-1/2 px-2">
                                     <button className="w-full bg-red-600 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteOnclick}>Delete Blog</button>
